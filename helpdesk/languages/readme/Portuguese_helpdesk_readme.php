e44a025ba3a3afe787723a1de0676f97bd46e797
rica-carv <rica-carv@users.noreply.github.com>
2025-04-16 21:53:15 +0100

    Added PT lanfiles

---
+++ helpdesk/languages/readme/Portuguese_helpdesk_readme.php
@@ -0,0 +1,27 @@
+<?php
+
+define("HDU_R01", "Leia-me");
+define("HDU_R02", "Plugin");
+define("HDU_R03", "");
+define("HDU_R04", "Autor");
+define("HDU_R05", "");
+define("HDU_R06", "Versão");
+define("HDU_R07", "");
+define("HDU_R08", "Estado");
+define("HDU_R09", "Lançamento");
+define("HDU_R10", "Isenção de responsabilidade");
+define("HDU_R11", "Não pode ser aceite qualquer responsabilidade pela falha deste plugin de qualquer forma ou maneira. Utiliza-o por tua conta e risco.");
+define("HDU_R12", "Copyright");
+define("HDU_R13", "Barry Keal 2007.");
+define("HDU_R14", "Licença");
+define("HDU_R15", "GPL");
+define("HDU_R16", "Website");
+define("HDU_R17", "www.keal.me.uk");
+define("HDU_R18", "Suporte");
+define("HDU_R19", "O suporte para este plugin está <strong>EXCLUSIVAMENTE</strong> disponível em <a href='http://www.keal.me.uk'>O meu website</a> e em mais nenhum outro local.");
+define("HDU_R20", "Documentação");
+define("HDU_R21", "Toda a documentação encontra-se no <a href='http://wiki.e107.org/?title=Helpdesk'>wiki do e107</a>. Sente-te à vontade para contribuir para a documentação nesse site.");
+define("HDU_R22", "Apenas para informação");
+define("HDU_R23", "Para edição, utilizo o PHP Edit da <a href='http://www.waterproof.fr/'>Waterproof Software</a>");
+define("HDU_R24", "Se achas que este plugin é útil, por favor considera fazer uma <b>doação</b> em <a href='http://www.keal.me.uk'>O meu website</a>");
+define("HDU_R25", "Doações");
